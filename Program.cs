477fe65fcf56f573760d5fe817707b4db74f7eee
pneu <pneumaster@gmail.com>
2015-12-30 21:04:39 +0900

    unittest

--- Program.cs
+++ Program.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace ForSonarQube
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args) {
+        }
+
+        public static int Sub(int a, int b, int c) {
+            if (a == 0) {
+                return b;
+            }
+            if (b == 0) {
+                return a;
+            }
+            return c;
         }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ForSonarQube;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            Assert.AreEqual(1, Program.Sub(0, 1, 2));
+            //Assert.AreEqual(3, Program.Sub(3, 0, 5));
+        }
+    }
+}
